01546dccbca59416cf19e4770dc4636c9ba26196
Manisha Kumawat <parjapatmanisha120.com>
2025-07-01 17:04:42 +0530

    Initial commit

---
+++ src/Details.js
@@ -0,0 +1,127 @@
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Project Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Personal Details
+export const personalDetails = {
+  name: "Manisha Kumawat",
+  tagline: "EEE Student | Web Developer | ML Intern",
+  img: profile,
+  about: "I am a passionate engineering student with experience in machine learning, web development, and embedded systems. I enjoy creating smart and impactful applications.",
+};
+
+// Social Media
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/manisha-kumawat",
+  github: "https://github.com/manisha123",
+  twitter: "",
+  instagram: "",
+};
+
+// Work Experience
+export const workDetails = [
+  {
+    Position: "Machine Learning Intern",
+    Company: "Bharat Intern",
+    Location: "Remote",
+    Type: "Internship",
+    Duration: "July 2024 - Aug 2024",
+  },
+  {
+    Position: "Python Developer Intern",
+    Company: "ESRC (Embedded System Robotics Club)",
+    Location: "College",
+    Type: "Internship",
+    Duration: "Jan 2024 - Mar 2024",
+  },
+  {
+    Position: "Intern",
+    Company: "Grid Substation (GSS)",
+    Location: "Jodhpur",
+    Type: "Internship",
+    Duration: "May 2023 - June 2023",
+  },
+];
+
+// Education Details
+export const eduDetails = [
+  {
+    Position: "BTech in Electrical and Electronics Engineering",
+    Company: "MBM University, Jodhpur",
+    Location: "Jodhpur",
+    Type: "Full Time",
+    Duration: "Aug 2021 - July 2026",
+  },
+];
+
+// Tech Stack
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Project Details
+export const projectDetails = [
+  {
+    title: "Movie Recommendation System",
+    image: projectImage1,
+    description: "A content-based movie recommender using Python and scikit-learn.",
+    techstack: "Python, Pandas, scikit-learn",
+    previewLink: "https://github.com/manisha123/movie-recommender",
+    githubLink: "https://github.com/manisha123/movie-recommender",
+  },
+  {
+    title: "House Price Prediction",
+    image: projectImage2,
+    description: "Regression model to predict house prices using ML algorithms.",
+    techstack: "Python, scikit-learn, Pandas",
+    previewLink: "https://github.com/manisha123/house-price-predictor",
+    githubLink: "https://github.com/manisha123/house-price-predictor",
+  },
+];
+
+// Contact Details
+export const contactDetails = {
+  email: "manisha@example.com",
+  phone: "+91 9876543210",
+};
+
+
